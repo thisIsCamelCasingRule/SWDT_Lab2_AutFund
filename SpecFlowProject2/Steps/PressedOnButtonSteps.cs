1292ac787c9f202fc15a65636a92ea80643cad9c
thisIsCamelCasingRule <nikitagrechyhov1@gmail.com>
2021-05-12 18:21:48 +0300

    initial

---
+++ SpecFlowProject2/Steps/PressedOnButtonSteps.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SWDT_AutFundHW.Steps
+{
+    [Binding]
+    public class PressedOnButtonSteps
+    {
+        [Given(@"the user is on home page")]
+        public void GivenTheUserIsOnHomePage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the user on services page")]
+        public void GivenTheUserOnServicesPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the services button is pressed")]
+        public void WhenTheServicesButtonIsPressed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the about button is pressed")]
+        public void WhenTheAboutButtonIsPressed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the our work button is pressed")]
+        public void WhenTheOurWorkButtonIsPressed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the insights button is pressed")]
+        public void WhenTheInsightsButtonIsPressed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the engineer button is pressed")]
+        public void WhenTheEngineerButtonIsPressed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the consult button is pressed")]
+        public void WhenTheConsultButtonIsPressed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the design button is pressed")]
+        public void WhenTheDesignButtonIsPressed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the operate button is pressed")]
+        public void WhenTheOperateButtonIsPressed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the page services appears")]
+        public void ThenThePageServicesAppears()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the page about appears")]
+        public void ThenThePageAboutAppears()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the page our work appears")]
+        public void ThenThePageOurWorkAppears()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the page insights appears")]
+        public void ThenThePageInsightsAppears()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the page engineer appears")]
+        public void ThenThePageEngineerAppears()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the page consult appears")]
+        public void ThenThePageConsultAppears()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the page design appears")]
+        public void ThenThePageDesignAppears()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the page operate appears")]
+        public void ThenThePageOperateAppears()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
